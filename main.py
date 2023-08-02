2b00226a5287ebbe3dde1522bbbfca705bd346e1
romu-lo <romulo.mincache@gmail.com>
2023-08-02 11:35:48 -0300

    alterar nível de habilidade do stockfish

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ def main():
     if len(sys.argv) == 2 and str(sys.argv[1]) == "-sf":
         print("sf")
     else:
-        nomeJogador = input("Digite o nome do jogador: ")
+        vsStockfish = int(input("Ativar Stockfish? (1 para sim e 0 para não): "))
         corSelecionada = int(
             input("Digite a cor que deseja jogar (1 para branco e 0 para preto): "))
-        vsStockfish = int(input("Ativar Stockfish? (1 para sim e 0 para não): "))
+        
+        if vsStockfish == 1:
+            nivelStockfish = int(input("Digite o nível de habilidade do Stockfish: "))
+            nomeJogador = 'stockfish_' + str(nivelStockfish) + ['_B' if corSelecionada == 1 else '_P'][0]
+        else:
+            nivelStockfish = 2000
+            nomeJogador = input("Digite o nome do jogador: ")
+
         if corSelecionada == 1:
-            chessGame = ChessGame(700, 750, 8, 8, True, nomeJogador, vsStockfish)
+            chessGame = ChessGame(700, 750, 8, 8, True, nomeJogador, vsStockfish, nivelStockfish)
         elif corSelecionada == 0:
-            chessGame = ChessGame(700, 750, 8, 8, False, nomeJogador, vsStockfish)
+            chessGame = ChessGame(700, 750, 8, 8, False, nomeJogador, vsStockfish, nivelStockfish)
+
         chessGame.desenharTabuleiro()
         chessGame.loopGame()
         chessGame.finalizar()
